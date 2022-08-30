693a48af9ab4768650234935e2ff6a02710b7078
unknown <harshasrit223@gmail.com>
2022-08-31 01:36:30 +0530

    end of section - 3 / Second Commit

---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,43 @@
+using Core.Entity;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly StoreContext _context;
+        public ProductRepository(StoreContext context)
+        {
+            _context = context;
+
+        }
+
+        public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
+        {
+           return await _context.ProductBrands.ToListAsync();
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+           return await _context.Products
+                                .Include(P=>P.ProductBrand)
+                                .Include(P=>P.ProductType)
+                                .FirstOrDefaultAsync(p=>p.Id == id);
+        }
+
+        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            return await _context.Products
+                                .Include(P=>P.ProductBrand)
+                                .Include(P=>P.ProductType)
+                                .ToListAsync();
+
+        }
+
+        public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
+        {
+           return await _context.ProductTypes.ToListAsync();
+        }
+    }
+}
